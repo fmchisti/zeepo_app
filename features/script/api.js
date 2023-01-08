7a060b58524832ee9f9821da2e7a96d41529d536
fmchisti <bd.fahim.bd@gmail.com>
2023-01-08 20:25:58 +0600

    api url change

--- features/script/api.js
+++ features/script/api.js
@@ -1,7 +1,5 @@
-let url = `https://us-central1-zeepo-dashboard-manager.cloudfunctions.net/api/script`;
-
 const getScriptData = async (locationid) => {
-  url = `${url}/${locationid}`;
+  let url = `https://us-central1-zeepo-dashboard-manager.cloudfunctions.net/api/script/${locationid}`;
   const response = await fetch(url);
   const result = await response.json();
   return result;
